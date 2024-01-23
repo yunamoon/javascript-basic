55c28386bc3c206b2563a342ba765313cb0ee035
yunamoon <tpwkao0051@gmail.com>
2024-01-23 20:13:42 +0900

    2023.01.23

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,33 @@
+/**
+ * variable 선언하기
+ * 
+ * 1) var _ 더 이상 사용하지 않는다.
+ * 2) let _ 
+ * 3) const 
+ */
+
+
+// let과 var은 추후 값을 변경 할 수 있다.
+var name = 'yuna moon';
+console.log(name);
+
+var age = 29;
+console.log(age);
+
+let sister = 'sena moon';
+console.log(sister);
+
+sister = 'hanna moon';
+console.log(sister);
+
+/**
+ * 선언과 할당.
+ * 
+ * 1) 변수 선언.
+ * 2) 변수에 값을 넣어주는 과정이 할당.
+ */
+
+// 선언
+var pet;
+// 할당
+pet = 'dog';
---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,27 @@
+/**
+ * Naming Conventions
+ * 
+ * 1) 일반적으로 영어를 사용하면 문자와 숫자를 모두 사용 할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러만 사용이 가능하다.
+ * 3) 숫자로 이름을 시작 할 수 없다.
+ * 4) 키워드는 변수명으로 사용 할 수 없다.
+ */
+
+let name = 'yuna moon';
+var $yuna = 'name';
+const _yuna = 'moon';
+
+console.log(name);
+console.log($yuna);
+console.log(_yuna);
+
+/**
+ * Naming Conventions 2
+ * 
+ * 1) camelCase = 대부분의 언어에서 많이 사용
+ * 2) snake_case = 언더 스코어로 단어 구분하고 소문자 사용
+ * 3) PascalCase = 단어의 시작을 대문자로 사용, c#이나 마이크로소프트 계열 언어에서 사용
+ */
+
+const moonYuNa = '문유나';
+console.log(moonYuNa);
