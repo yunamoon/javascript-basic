b6f2b358d1cb27765a3bbc7933e8fc356a8f1509
yunamoon <tpwkao0051@gmail.com>
2024-01-24 16:57:20 +0900

    2024.01.24

---
+++ 1_basics/5_data_types.js
@@ -0,0 +1,33 @@
+/**
+ * Data Types
+ * 
+ * 여섯개의 Primitive 타입과 
+ * 한개의 오브젝트 타입이 있습니다.
+ * 
+ * 1) Number 
+ * 2) String
+ * 3) Boolean
+ * 4) Undefined 
+ * 5) Null
+ * 6) Symbol 
+ * 
+ * 7) Object
+ *    Function
+ *    Array
+ *    Object
+ */
+
+//number
+const age = 29;
+const pi = 3.14;
+
+console.log(typeof age , typeof pi);
+
+const infinity = Infinity;
+const nInfinity = -Infinity;
+console.log(typeof infinity , typeof nInfinity);
+
+//string
+const name = '문유니';
+console.log(typeof name);
+
